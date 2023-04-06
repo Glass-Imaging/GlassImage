79174e0cef3ebcdd4c8a98593d37e1041c5d1877
Fabio Riccardi <fabio@glass-imaging.com>
2023-04-05 17:12:35 -0700

    Updated copyright notices.

--- include/gls_logging.h
+++ include/gls_logging.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2022 Glass Imaging Inc.
+// Copyright (c) 2021-2023 Glass Imaging Inc.
 // Author: Fabio Riccardi <fabio@glass-imaging.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
