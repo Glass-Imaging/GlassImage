6ad21f6175cb10ca6618bda5d2b30c4a59c7df38
mako443 <manuel.kolmet@gmail.com>
2025-08-18 12:52:01 +0200

    Tested GpuBuffer with custom type.

--- tests/unit/gpu_buffer_test.cpp
+++ tests/unit/gpu_buffer_test.cpp
@@ -6,7 +6,36 @@
 #include <string>
 #include <vector>
 
+#include "glass_image/gpu_kernel.h"
+#include "testing_kernels.h"
+
 using std::vector, std::span;
+
+typedef struct
+{
+    int int_value;
+    float float_value;
+} CustomBufferStruct;
+
+class CustomBufferAddKernel : gls::GpuKernel
+{
+   public:
+    CustomBufferAddKernel(std::shared_ptr<gls::OCLContext> gpu_context)
+        : gls::GpuKernel(gpu_context, "CustomBufferAddKernel")
+    {
+    }
+
+    cl::Event operator()(gls::GpuBuffer<CustomBufferStruct> buffer,
+                         std::optional<cl::CommandQueue> queue = std::nullopt,
+                         const std::vector<cl::Event>& events = {})
+    {
+        SetArgs(buffer.buffer());
+        cl::CommandQueue _queue = queue.value_or(gpu_context_->clCommandQueue());
+        cl::Event event;
+        _queue.enqueueNDRangeKernel(kernel_, {}, {buffer.size, 1, 1}, {}, &events, &event);
+        return event;
+    }
+};
 
 TEST(GpuBufferTest, CreateFromSpan_ToVector)
 {
@@ -65,4 +94,28 @@ TEST(GpuBufferTest, MapBuffer)
     std::vector<float> result = buffer.ToVector();
     EXPECT_EQ(result.size(), 4);
     for (int i = 0; i < result.size(); i++) EXPECT_EQ(i, result[i]);
+}
+
+TEST(GpuBufferTest, CustomBufferType)
+{
+    std::vector<std::string> kernel_sources{testing_kernel_code};
+    auto gpu_context = std::make_shared<gls::OCLContext>(kernel_sources, "");
+    gpu_context->loadProgramsFromFullStringSource(kernel_sources, "");
+
+    gls::GpuBuffer<CustomBufferStruct> buffer(gpu_context, 4);
+
+    auto mapped = buffer.MapBuffer();
+    for (int i = 0; i < buffer.size; i++)
+    {
+        mapped->data_[i].int_value = i;
+        mapped->data_[i].float_value = 0.2f;
+    }
+    mapped.reset();
+
+    CustomBufferAddKernel kernel(gpu_context);
+    kernel(buffer).wait();
+
+    std::vector<CustomBufferStruct> result = buffer.ToVector();
+    EXPECT_EQ(result.size(), 4);
+    for (int i = 0; i < result.size(); i++) EXPECT_EQ(result[i].float_value, i + 0.2f);
 }
--- tests/unit/testing_kernels.h
+++ tests/unit/testing_kernels.h
@@ -1,10 +1,22 @@
 const char* testing_kernel_code = R"(
 
+typedef struct {
+    int int_value;
+    float float_value;
+} CustomBufferStruct;
+
 const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
 
 __kernel void BufferAddKernel(__global float* data, float value) {
     int x = get_global_id(0);
     data[x] += value;
+}
+
+__kernel void CustomBufferAddKernel(__global CustomBufferStruct* data){
+    int x = get_global_id(0);
+    CustomBufferStruct s = data[x];
+    s.float_value = s.float_value + s.int_value;
+    data[x] = s;
 }
 
 __kernel void ImageAddKernel(read_only image2d_t image, float value, write_only image2d_t output){
