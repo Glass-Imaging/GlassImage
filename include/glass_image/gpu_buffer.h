486b2bb662069187eb581758c8b745282289cccd
mako443 <manuel.kolmet@gmail.com>
2025-08-18 12:30:33 +0200

    GpuBuffer now header only.

--- include/glass_image/gpu_buffer.h
+++ include/glass_image/gpu_buffer.h
@@ -27,26 +27,77 @@ struct MappedBuffer final
     std::function<void()> cleanup_;
 };
 
+/// We implement GpuBuffer as a header-only library in order to allow for flexible, potentially struct-based buffer
+/// types.
 template <typename T>
 class GpuBuffer
 {
    public:
-    GpuBuffer(std::shared_ptr<gls::OCLContext> gpu_context, const size_t size, cl_mem_flags flags = CL_MEM_READ_WRITE);
+    GpuBuffer(std::shared_ptr<gls::OCLContext> gpu_context, const size_t size, cl_mem_flags flags = CL_MEM_READ_WRITE)
+        : gpu_context_(gpu_context), size(size)
+    {
+        /// TODO: pitch for image? Or create croppable "buffer-based" images?
+        buffer_ = cl::Buffer(gpu_context->clContext(), flags, sizeof(T) * size);
+    };
+
     GpuBuffer(std::shared_ptr<gls::OCLContext> gpu_context, const std::span<T>& data,
-              cl_mem_flags flags = CL_MEM_READ_WRITE);
+              cl_mem_flags flags = CL_MEM_READ_WRITE)
+        : gpu_context_(gpu_context), size(data.size())
+    {
+        buffer_ = cl::Buffer(gpu_context->clContext(), flags | CL_MEM_COPY_HOST_PTR, sizeof(T) * size, data.data());
+    };
 
     std::vector<T> ToVector(std::optional<cl::CommandQueue> queue = std::nullopt,
-                            const std::vector<cl::Event>& events = {});
+                            const std::vector<cl::Event>& events = {})
+    {
+        cl::CommandQueue _queue = queue.value_or(gpu_context_->clCommandQueue());
+        std::vector<T> data(size);
+        _queue.enqueueReadBuffer(buffer_, CL_TRUE, 0, size * sizeof(T), data.data(), &events);
+        return data;
+    };
 
     cl::Event CopyFrom(const std::span<T>& data, std::optional<cl::CommandQueue> queue = std::nullopt,
-                       const std::vector<cl::Event>& events = {});
+                       const std::vector<cl::Event>& events = {})
+    {
+        if (data.size() != size)
+            throw std::runtime_error(std::format("LoadVector() expected data of size {}, got {}.", size, data.size()));
+
+        cl::CommandQueue _queue = queue.value_or(gpu_context_->clCommandQueue());
+        cl::Event event;
+        _queue.enqueueWriteBuffer(buffer_, CL_FALSE, 0, size * sizeof(T), data.data(), &events, &event);
+        return event;
+    };
 
     cl::Event CopyTo(std::span<T>& data, std::optional<cl::CommandQueue> queue = std::nullopt,
-                     const std::vector<cl::Event>& events = {});
+                     const std::vector<cl::Event>& events = {})
+    {
+        if (data.size() != size)
+            throw std::runtime_error(std::format("LoadVector() expected data of size {}, got {}.", size, data.size()));
+
+        cl::CommandQueue _queue = queue.value_or(gpu_context_->clCommandQueue());
+        cl::Event event;
+        _queue.enqueueReadBuffer(buffer_, CL_FALSE, 0, size * sizeof(T), data.data(), &events, &event);
+        return event;
+    }
 
     /// TODO: Can't I use a custom deleter instead?!
     std::unique_ptr<MappedBuffer<T>> MapBuffer(std::optional<cl::CommandQueue> queue = std::nullopt,
-                                               const std::vector<cl::Event>& events = {});
+                                               const std::vector<cl::Event>& events = {})
+    {
+        if (is_mapped_) throw std::runtime_error("MapBuffer() called on a buffer that is already mapped.");
+
+        cl::CommandQueue _queue = queue.value_or(gpu_context_->clCommandQueue());
+        void* ptr = _queue.enqueueMapBuffer(buffer_, CL_TRUE, CL_MAP_READ | CL_MAP_WRITE, 0, size * sizeof(T));
+        auto cleanup = [this, ptr, &_queue, &events]()
+        {
+            _queue.enqueueUnmapMemObject(buffer_, ptr, &events);
+            is_mapped_ = false;
+        };
+        std::span<T> data_span(static_cast<T*>(ptr), size);
+
+        is_mapped_ = true;
+        return std::make_unique<MappedBuffer<T>>(data_span, cleanup);
+    };
 
     const size_t size;
     size_t ByteSize() { return size * sizeof(T); };
